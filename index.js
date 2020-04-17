efaaa60a8f87ebdf0c77c891ee9292682cc8d418
Amber-Pittman <amber@amberpittman.com>
2020-04-17 13:17:13 -0400

    Added global 404 middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
 const express = require("express")
 const cors = require("cors")
+// const morgan = require("morgan")
 
 const postRouter = require("./posts/postRouter")
 const userRouter = require("./users/userRouter")
@@ -10,9 +11,16 @@ const port = 8080;
 
 server.use(express.json())
 server.use(cors())
+// server.use(morgan())
 
 server.use("/posts", postRouter)
 server.use("/users", userRouter)
+
+server.use((req, res) => {
+    res.status(404).json({
+        message: "Route was not found.",
+    })
+})
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
