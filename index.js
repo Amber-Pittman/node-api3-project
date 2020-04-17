4bcbe94eec5392b99f32f49f07d756e836904955
Amber-Pittman <amber@amberpittman.com>
2020-04-17 13:00:35 -0400

    Basic index file set up

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+const express = require("express")
+const cors = require("cors")
+
+const postRouter = require("./posts/postRouter")
+
+const server = express()
+const port = 8080;
+
+server.use(express.json())
+server.use(cors())
+
+server.use("/posts", postRouter)
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
