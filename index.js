32ac63b32f2f61883bbe4987997e07e53508423f
Amber-Pittman <amber@amberpittman.com>
2020-04-17 13:02:47 -0400

    Added userRouter to index file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cors = require("cors")
 
 const postRouter = require("./posts/postRouter")
+const userRouter = require("./users/userRouter")
 
 const server = express()
 const port = 8080;
@@ -11,6 +12,7 @@ server.use(express.json())
 server.use(cors())
 
 server.use("/posts", postRouter)
+server.use("/users", userRouter)
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
